e8bf6c5ca6569045c3874eb61adc76088b022b4c
mahbubHossen-dev <mhbabu2002@gmail.com>
2025-04-14 15:28:42 +0600

    first commit

---
+++ config/db.js
@@ -0,0 +1,74 @@
+import mongoose from "mongoose";
+
+const cached = global.mongoose
+
+if(!cached){
+    cached = global.mongoose = {conn: null, promise: null}
+}
+
+async function connectDB(){
+    if(cached){
+        return cached.conn
+    }
+
+    if(!promise){
+        const opts = {
+            bufferCommands: false
+        }
+
+        cached.promise = mongoose.connect(`${process.env.MONGODB_URI}/QuickCart`, opts)
+    }
+
+    cached.conn = await cached.promise
+
+    return cached.conn
+}
+
+export default connectDB
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import mongoose from "mongoose";
+
+// const cached = global.mongoose
+
+// if (!cached) {
+//     catched = global.mongose = { conn: null, promise: null }
+// }
+
+// async function connectDB() {
+//     if (cached.conn) {
+//         return cached.conn
+//     }
+
+//     if (!cached.promise) {
+//         const opts = {
+//             bufferCommands: false
+//         }
+
+//         cached.promise = mongoose.connect(`${process.env.MONGODB_URI}/QuickCart`, opts).then(mongoose => {
+//             return mongoose
+//         })
+//     }
+
+//     cached.conn = await cached.promise
+
+//     return cached.conn
+// }
+
+// export default connectDB;
